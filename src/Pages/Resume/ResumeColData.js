ba07a776c6fab9f20a33c62115494e03492b9384
Hamza Nasir <myselfhamzanasir@gmail.com>
2025-02-26 10:26:33 +0500

    First Commit

---
+++ src/Pages/Resume/ResumeColData.js
@@ -0,0 +1,60 @@
+// Education Data
+export const educationData = [
+    {
+        id: 1,
+        tagLine: "March 2022 - 2023",
+        headline: "Frontend Web Development",
+        para:"Completed Frontend Development at SMIT, mastering HTML, CSS, and JS and React.js to create responsive and dynamic web applications.",
+        uniPara: "SMIT (Saylani Mass It Training)",
+    },
+    {
+        id: 2,
+        tagLine: "March 2023 - 2024",
+        headline: "Backend Web Development",
+        para:"Completed MERN Stack Development, mastering React.js, MongoDB, Express.js, React.js, and Node.js to build scalable, responsive, and dynamic web applications.",
+        uniPara: "Sheryans Coding School",
+    },
+    {
+        id: 3,
+        tagLine: " August 2023 - Present",
+        headline: "BSCS (Bachelor of Science in Computer Science)",
+        para: "Currently pursuing a BS in Computer Science, building advanced knowledge and skills in software development for future success.",
+        uniPara: "UAF (University if Agriculture Faisalabad)",
+    },
+    {
+        id: 4,
+        tagLine: "September 2021 - 2023",
+        headline: "ICS (Intermidiate in Computer Science)",
+        para: "Successfully completed Intermediate in Computer Science, gaining a strong foundation in programming and analytical skills for future growth.",
+        uniPara: "Kips College",
+    },
+    {
+        id: 5,
+        tagLine: "2019 - 2021",
+        headline: "Matriculation",
+        para: "Successfully completed matriculation, building a strong academic foundation and essential skills for future growth.",
+        uniPara: "Dar-e-Arqam School",
+    },
+  
+]
+
+
+// Experience Data
+export const experienceData = [
+    {
+        id: 1,
+        tagLine: "March 2022 - Present",
+        headline: "MERN Stack Web Development",
+        para: "MERN Stack Developer skilled in React.js, Node.js, Express.js, and MongoDB, building responsive and dynamic web apps. I craft modern UI/UX with GSAP, SCSS, ensuring performance and functionality. Open to freelance projects and collaborations.",
+        uniPara: "SMIT & Sheryan Coding School",
+    },
+    {
+        id: 2,
+        tagLine: " June 2024 - Present",
+        headline: "UI UX Designer",
+        para: "Experienced in UI/UX design, focusing on creating intuitive and visually appealing user interfaces and enhancing user experiences.",
+        uniPara: "Snow Dreams Studios",
+    },
+]
+
+
---
+++ src/Pages/Testimonial/slidesData.js
@@ -0,0 +1,26 @@
+import ahmad from "../../assets/images/pic.jfif"
+import anas from "../../assets/images/anas.jpg"
+import usman from "../../assets/images/usman.jpg"
+export const slideData = [
+    {
+        id: 1,
+        para: "The experience of working with Hamza was excellent. Their proficiency with front-end development allowed us to turn our concept into an engaging, dynamic website. The site was distinguished by its flexible design and meticulous attention to detail. Strongly suggest to anyone searching for a capable and experienced developer.",
+        img: ahmad,
+        name: "Ahmad Nasir",
+        work: "CopyWriting",
+    },
+    {
+        id: 2,
+        para: "It was a pleasure working with you on a challenging project that involved interactive online elements. Their expertise with GSAP and React.js gave our application a new degree of interactivity. They impressed me with their professionalism and ability to meet deadlines. I'm excited to collaborate with them once more.",
+        img: anas,
+        name: "Anas Iftikhar",
+        work: "Software Engineer",
+    },
+    {
+        id: 3,
+        para: "Working with Hamza Nasir was a transformative experience for our project. They expertly translated our complex UI/UX designs into a fully functional and visually appealing website. Their attention to detail and ability to bring interactive elements to life significantly improved our user experience. I am extremely satisfied with their work and highly recommend them for any UI/UX development needs!",
+        img: usman,
+        name: "Usman Kashif",
+        work: "UI UX Designer",
+    },
+]
