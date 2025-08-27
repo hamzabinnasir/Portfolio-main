421fa5af288b28346bff277aa04deb3757082b19
Hamza Bin Nasir <141481692+hamzabinnasir@users.noreply.github.com>
2025-08-27 16:32:45 +0500

    Update Footer.jsx

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -13,16 +13,13 @@ export default function Footer() {
                 <p>Copyright © 2024 All rights reserved | This template is made <FavoriteIcon className="heartIcon" /> by Hamza Bin Nasir</p>
                 <div className="footerIcoContainer">
                     <div className="iconDiv">
-                        <a className="footerLink" href="https://www.linkedin.com/in/hamza-nasir-771161287/"><FaLinkedin className="footerIcon" /></a>
+                        <a className="footerLink" href="https://www.linkedin.com/in/hamza-nasir-183a4637b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><FaLinkedin className="footerIcon" /></a>
                     </div>
                     <div className="iconDiv">
                         <a className="footerLink" href="https://github.com/hamzabinnasir"><GitHubIcon className="footerIcon" /></a>
                     </div>
                     <div className="iconDiv">
                         <a className="footerLink" href="https://www.facebook.com/profile.php?id=61575199420536"><FacebookIcon className="footerIcon"/></a>
-                    </div>
-                    <div className="iconDiv">
-                        <a className="footerLink" href="https://www.fiverr.com/sellers/hmza_developer2/edit"><img className="fiverIcon" src={fiverIcon} alt="no img" /></a>
                     </div>
                 </div>
             </footer>
